d652b1fddca6290035704bcc493a787e4bb86f2d
SivaganeshSelvam23 <sivaganesh.selvam.2324@gmail.com>
2026-06-28 19:54:22 +0100

    Admin Enable/Disable Users changes commit

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -31,3 +31,66 @@ export const getAllUsers = async (req, res) => {
     });
   }
 };
+
+export const updateUserStatus = async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+    const { isActive } = req.body;
+
+    // 1. Validate user ID
+    if (!Number.isInteger(userId) || userId <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "A vaild user ID is required.",
+      });
+    }
+
+    // 2. Validate request body
+    if (typeof isActive !== "boolean") {
+      return res.status(400).json({
+        success: false,
+        message: "isActive must be a boolean value.",
+      });
+    }
+    // 3. Update user status
+    const updatedUserResult = await pool.query(
+      `
+        UPDATE users
+        SET
+          is_active = $1,
+          updated_at = CURRENT_TIMESTAMP
+        WHERE id = $2
+        RETURNING
+          id,
+          name,
+          email,
+          role,
+          is_active,
+          created_at,
+          updated_at
+      `,
+      [isActive, userId],
+    );
+
+    // 4. Handling missing user
+    if (updatedUserResult.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+    const updatedUser = updatedUserResult.rows[0];
+    // 5. Return updated user
+    return res.status(200).json({
+      success: true,
+      message: `User account ${updatedUser.is_active ? "enabled" : "disabled"} successfully.`,
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.log("Failed t update user status", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error.",
+    });
+  }
+};
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { authenticate } from "../middlewares/authenticate.js";
 import { authorize } from "../middlewares/authorize.js";
-import { getAllUsers } from "../controllers/adminController.js";
+import {
+  getAllUsers,
+  updateUserStatus,
+} from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.get("/users", authenticate, authorize("admin"), getAllUsers);
+router.patch(
+  "/users/:id/status",
+  authenticate,
+  authorize("admin"),
+  updateUserStatus,
+);
 
 export default router;
